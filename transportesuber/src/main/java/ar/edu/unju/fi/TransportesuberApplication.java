4a2b576251709354eab8c8cf15eac00ad5d42aa3
Usuario <Usuario@DESKTOP-K7IO1I1>
2025-02-18 06:17:59 -0300

    generate spring boot Proyect

---
+++ transportesuber/src/main/java/ar/edu/unju/fi/TransportesuberApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportesuberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportesuberApplication.class, args);
+	}
+
+}
